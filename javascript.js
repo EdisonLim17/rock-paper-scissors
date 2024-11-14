9e50161a233b07ed9e7f27cd10fbf41df176dfce
Edison Lim <117683125+EdisonLim17@users.noreply.github.com>
2024-11-14 02:41:13 -0500

    Added game logic for one round

--- javascript.js
+++ javascript.js
@@ -1,17 +1,30 @@
+let humanScore = 0, computerScore = 0;
+
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     let choice = (random === 0) ? "rock" : (random === 1) ? "paper" : "scissors";
-    console.log(choice);
+    return choice;
 }
 
 function getHumanChoice() {
     let choice = "invalid";
     while(choice === "invalid"){
-        let input = prompt("Rock (r), Paper (p), or Scissors (s)?")
-        choice = (input === "r") ? "rock" : (input === "p") ? "paper" : (input === "s") ? "scissors" : "invalid";
+        let input = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        choice = (input === "rock") ? "rock" : (input === "paper") ? "paper" : (input === "scissors") ? "scissors" : "invalid";
     }
-    console.log(choice);
+    return choice;
 }
 
-getComputerChoice();
-getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Tie!");
+    } else if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You Win! " + humanChoice + " beats " + computerChoice + "!");
+        humanScore++;
+    } else {
+        console.log("You Lose! " + computerChoice + " beats " + humanChoice + "!");
+        computerScore++;
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
